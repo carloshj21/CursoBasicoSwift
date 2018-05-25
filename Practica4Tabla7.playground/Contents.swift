a6c0af8fee372d71d69a92871fa3b8a495289d6d
carloshj21 <carlos21p6-0800@comunidad.unam.mx>
2018-05-25 14:38:25 -0500

    Práctica 4 tabla 7

---
+++ Practica4Tabla7.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var contador = 0
+var tabla = 7
+
+for contador in 0...10 {
+    print(tabla * contador)
+}
---
+++ Practica5Pares.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+for cont in 1...10 {
+    if(cont % 2 == 0 && cont != 8 && cont != 10){
+            print(cont)
+    }
+}
