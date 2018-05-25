c16ace1d2537009e083ae2fb58e07d81c1cf2575
carloshj21 <carlos21p6-0800@comunidad.unam.mx>
2018-05-25 13:38:28 -0500

    Clase 2

---
+++ Practica3.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var pal1 = "radar"
+
+var pal2 = String(pal1.reversed())
+
+if (pal1 == pal2) {
+    print("Es un palíndromo")
+}
+else {
+    print("No es un palíndromo")
+}
+
